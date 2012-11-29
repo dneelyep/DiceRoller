db42081288d891c74abb6327a94009421dc61e84
Daniel Neel <dneelyep@gmail.com>
2012-11-28 20:01:48 -0500

    A lot of minor code cleanups, removing unneeded data structures, etc.

--- src/DiceRoller.java
+++ src/DiceRoller.java
@@ -9,15 +9,6 @@ import com.dneelyep.utils.GridBagUtils;
 /** Simple program to automatically roll dice in a D and D game. */
 public class DiceRoller extends JFrame {
 
-    /** The number of dice allowed in the spinners. */
-    private Integer[] nums = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
-
-    /** A Map linking together the die controllers and the panels that display their randomized output. */
-    private Map<JSpinner, JPanel> controllerPanelMap = new HashMap() {{
-        for (dieTypes type : dieTypes.values())
-            put(type.getController(), new JPanel());
-    }};
-
     public static void main(String[] args) {
         new DiceRoller();
     }
@@ -30,19 +21,13 @@ public class DiceRoller extends JFrame {
         add(panel);
 
         GridBagConstraints constraints = new GridBagConstraints();
-        constraints.ipadx = 20;
-        constraints.ipady = 10;
-
-        GridBagUtils.addAtCoords(new JLabel("d3:"), new Point(0, 0), panel, constraints);
-        GridBagUtils.addAtCoords(new JLabel("d4:"), new Point(0, 1), panel, constraints);
-        GridBagUtils.addAtCoords(new JLabel("d6:"), new Point(0, 2), panel, constraints);
-        GridBagUtils.addAtCoords(new JLabel("d8:"), new Point(0, 3), panel, constraints);
-        GridBagUtils.addAtCoords(new JLabel("d10:"), new Point(0, 4), panel, constraints);
-        GridBagUtils.addAtCoords(new JLabel("d12:"), new Point(0, 5), panel, constraints);
-        GridBagUtils.addAtCoords(new JLabel("d16:"), new Point(0, 6), panel, constraints);
-        GridBagUtils.addAtCoords(new JLabel("d20:"), new Point(0, 7), panel, constraints);
+        constraints.ipadx = 40;
+        constraints.ipady = 40;
 
         for (dieTypes type : dieTypes.values()) {
+            GridBagUtils.addAtCoords(new JLabel(type.toString() + ": "),
+                    new Point(0, (Integer) type.getController().getClientProperty("y")), panel, constraints);
+
             GridBagUtils.addAtCoords(type.getController(), new Point((Integer) type.getController().getClientProperty("x"),
                     (Integer) type.getController().getClientProperty("y")), panel, constraints);
         }
@@ -60,7 +45,7 @@ public class DiceRoller extends JFrame {
         resetButton.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-                reset(panel);
+                reset();
             }
         });
         GridBagUtils.addAtCoords(resetButton, new Point(1, 8), panel, constraints);
@@ -77,52 +62,22 @@ public class DiceRoller extends JFrame {
 
         resultsFrame.add(resultsPanel);
 
-        controllerPanelMap.get(dieTypes.d3.getController()).add(new JLabel("d3: "));
-        controllerPanelMap.get(dieTypes.d4.getController()).add(new JLabel("d4: "));
-        controllerPanelMap.get(dieTypes.d6.getController()).add(new JLabel("d6: "));
-        controllerPanelMap.get(dieTypes.d8.getController()).add(new JLabel("d8: "));
-        controllerPanelMap.get(dieTypes.d10.getController()).add(new JLabel("d10: "));
-        controllerPanelMap.get(dieTypes.d12.getController()).add(new JLabel("d12: "));
-        controllerPanelMap.get(dieTypes.d16.getController()).add(new JLabel("d16: "));
-        controllerPanelMap.get(dieTypes.d20.getController()).add(new JLabel("d20: "));
+        for (dieTypes type : dieTypes.values()) {
+            type.getPanel().add(new JLabel(type.toString() + ": "));
+            resultsPanel.add(type.getPanel());
 
-        for (JPanel panel : controllerPanelMap.values()) {
-            resultsPanel.add(panel);
-        }
-
-        for (JSpinner spinner : controllerPanelMap.keySet()) {
-
-            if (! spinner.getValue().equals(0)) {
-
-                for (int i = 0; i < (Integer) spinner.getValue(); i++) {
-                    // TODO Simplify this much
-                    if (spinner == dieTypes.d3.getController())
-                        controllerPanelMap.get(dieTypes.d3.getController()).add(new JLabel(Integer.toString((Math.abs(new Random().nextInt() % 3) + 1))));
-                    else if (spinner == dieTypes.d4.getController())
-                        controllerPanelMap.get(dieTypes.d4.getController()).add(new JLabel(Integer.toString((Math.abs(new Random().nextInt() % 4) + 1))));
-                    else if (spinner == dieTypes.d6.getController())
-                        controllerPanelMap.get(dieTypes.d6.getController()).add(new JLabel(Integer.toString((Math.abs(new Random().nextInt() % 6) + 1))));
-                    else if (spinner == dieTypes.d8.getController())
-                        controllerPanelMap.get(dieTypes.d8.getController()).add(new JLabel(Integer.toString((Math.abs(new Random().nextInt() % 8) + 1))));
-                    else if (spinner == dieTypes.d10.getController())
-                        controllerPanelMap.get(dieTypes.d10.getController()).add(new JLabel(Integer.toString((Math.abs(new Random().nextInt() % 10) + 1))));
-                    else if (spinner == dieTypes.d12.getController())
-                        controllerPanelMap.get(dieTypes.d12.getController()).add(new JLabel(Integer.toString((Math.abs(new Random().nextInt() % 12) + 1))));
-                    else if (spinner == dieTypes.d16.getController())
-                        controllerPanelMap.get(dieTypes.d16.getController()).add(new JLabel(Integer.toString((Math.abs(new Random().nextInt() % 16) + 1))));
-                    else if (spinner == dieTypes.d20.getController())
-                        controllerPanelMap.get(dieTypes.d20.getController()).add(new JLabel(Integer.toString((Math.abs(new Random().nextInt() % 20) + 1))));
-                }
+            if (!type.getController().getValue().equals(0)) {
+                for (int i = 0; i < (Integer) type.getController().getValue(); i++)
+                    type.getPanel().add(new JLabel(Integer.toString((Math.abs(new Random().nextInt() % type.getValue()) + 1))));
             }
         }
-
 
         JButton closeButton = new JButton("Close");
         closeButton.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-                for (JPanel panel : controllerPanelMap.values())
-                    panel.removeAll();
+                for (dieTypes type : dieTypes.values())
+                    type.getPanel().removeAll();
 
                 resultsFrame.setVisible(false);
             }
@@ -135,16 +90,8 @@ public class DiceRoller extends JFrame {
     }
 
     /** Reset the all dice values in the provided panel. */
-    private void reset(JPanel panel) {
+    private void reset() {
         for (dieTypes dieType : dieTypes.values())
             dieType.getController().setValue(0);
-    }
-
-    // TODO Move this to the enum?
-    /** Initialize a given component at the point (x, y). */
-    protected static JComponent initAtCoords(JComponent component, int x, int y) {
-        component.putClientProperty("x", x);
-        component.putClientProperty("y", y);
-        return component;
     }
 }
--- src/dieTypes.java
+++ src/dieTypes.java
@@ -20,22 +20,31 @@ import java.util.Arrays;
 
 /** An enumeration of all the types of die available to roll. */
 public enum dieTypes {
-    d3(3,   (JSpinner) DiceRoller.initAtCoords(new JSpinner(new SpinnerListModel(Arrays.asList(0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10))), 1, 0)),
-    d4(4,   (JSpinner) DiceRoller.initAtCoords(new JSpinner(new SpinnerListModel(Arrays.asList(0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10))), 1, 1)),
-    d6(6,   (JSpinner) DiceRoller.initAtCoords(new JSpinner(new SpinnerListModel(Arrays.asList(0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10))), 1, 2)),
-    d8(8,   (JSpinner) DiceRoller.initAtCoords(new JSpinner(new SpinnerListModel(Arrays.asList(0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10))), 1, 3)),
-    d10(10, (JSpinner) DiceRoller.initAtCoords(new JSpinner(new SpinnerListModel(Arrays.asList(0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10))), 1, 4)),
-    d12(12, (JSpinner) DiceRoller.initAtCoords(new JSpinner(new SpinnerListModel(Arrays.asList(0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10))), 1, 5)),
-    d16(16, (JSpinner) DiceRoller.initAtCoords(new JSpinner(new SpinnerListModel(Arrays.asList(0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10))), 1, 6)),
-    d20(20, (JSpinner) DiceRoller.initAtCoords(new JSpinner(new SpinnerListModel(Arrays.asList(0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10))), 1, 7));
+    d3(3,   "d3",  new JPanel(), (JSpinner) giveCoords(new JSpinner(new SpinnerListModel(Arrays.asList(0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10))), 1, 0)),
+    d4(4,   "d4",  new JPanel(), (JSpinner) giveCoords(new JSpinner(new SpinnerListModel(Arrays.asList(0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10))), 1, 1)),
+    d6(6,   "d6",  new JPanel(), (JSpinner) giveCoords(new JSpinner(new SpinnerListModel(Arrays.asList(0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10))), 1, 2)),
+    d8(8,   "d8",  new JPanel(), (JSpinner) giveCoords(new JSpinner(new SpinnerListModel(Arrays.asList(0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10))), 1, 3)),
+    d10(10, "d10", new JPanel(), (JSpinner) giveCoords(new JSpinner(new SpinnerListModel(Arrays.asList(0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10))), 1, 4)),
+    d12(12, "d12", new JPanel(), (JSpinner) giveCoords(new JSpinner(new SpinnerListModel(Arrays.asList(0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10))), 1, 5)),
+    d16(16, "d16", new JPanel(), (JSpinner) giveCoords(new JSpinner(new SpinnerListModel(Arrays.asList(0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10))), 1, 6)),
+    d20(20, "d20", new JPanel(), (JSpinner) giveCoords(new JSpinner(new SpinnerListModel(Arrays.asList(0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10))), 1, 7));
 
     private final int value;
 
     /** Spinner to change the number of this die type that the user is rolling. */
     private final JSpinner controller;
 
-    dieTypes(int value, JSpinner controller) {
+    /** Panel that displays the random result of a given dieType being rolled. */
+    private final JPanel resultPanel;
+
+    // TODO Just derive this from value?
+    /** This dieType's String representation. */
+    private final String asString;
+
+    dieTypes(int value, String asString, JPanel resultPanel, JSpinner controller) {
         this.value = value;
+        this.asString = asString;
+        this.resultPanel = resultPanel;
         this.controller = controller;
     }
 
@@ -47,4 +56,20 @@ public enum dieTypes {
         return controller;
     }
 
+    public JPanel getPanel() {
+        return resultPanel;
+    }
+
+    @Override
+    /** Get a string representation of this dieType. */
+    public String toString() {
+        return asString;
+    }
+
+    /** Initialize a given component at the point (x, y). */
+    private static JComponent giveCoords(JComponent component, int x, int y) {
+        component.putClientProperty("x", x);
+        component.putClientProperty("y", y);
+        return component;
+    }
 }
